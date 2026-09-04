921035fc7208bb3c12a21e90615bed057a94f667
Sheri2005 <matiassanchezcr@gmail.com>
2026-09-03 19:43:22 -0500

    first commit

---
+++ js/data.js
@@ -0,0 +1,7 @@
+const menu = [
+    {nombre: "Hamburguesa", precio: 5.99},
+    {nombre: "Pizza", precio: 8.99},
+    {nombre: "Ensalada", precio: 4.99},
+    {nombre: "Pasta", precio: 7.99},
+    {nombre: "Sopa", precio: 3.99}
+];
